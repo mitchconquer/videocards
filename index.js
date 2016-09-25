d8ce849947821193090b2617b7f32c78d59ad105
mitchconquer <mitch@robotshanghai.com>
2016-09-24 18:48:45 -0700

    Create basic mp3 of fixed size

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 #! /usr/bin/env node
 
-const userArgs = process.argv.slice(2);
+const ffmpeg = require('fluent-ffmpeg');
 
-console.log({userArgs});
+const userArgs = process.argv.slice(2);
+const inputFile = userArgs[0];
+const inputStart = '5:00';
+const duration = '00:30.000';
+
+ffmpeg(inputFile)
+  .seekInput(inputStart)
+  .inputOptions('-vn')
+  .output(`output/audio-${inputFile}-${inputStart}.mp3`)
+  .format('mp3')
+  .duration(duration)
+
+  .on('error', (err) => {
+    console.log('An error occurred: ' + err.message);
+  })
+  .on('end', () => {
+    console.log(`Processing ${inputFile} finished !`);
+  })
+  .run();
 
