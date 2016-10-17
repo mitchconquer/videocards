a564d3d2a095f0d97be2e18925dae7f492c8e1e2
Mitch Conquer <mitch.conquer@gonimbly.com>
2016-10-16 18:00:50 -0700

    Basic prompt

--- src/initialize.js
+++ src/initialize.js
@@ -1,5 +1,6 @@
 const ffmpeg = require('fluent-ffmpeg');
 const Bromise = require('bluebird');
+const inquirer = require('inquirer');
 
 const initialize = (inputSubs, inputVideo) => {
   if (inputSubs) {
@@ -10,14 +11,35 @@ const initialize = (inputSubs, inputVideo) => {
       });
     });
   }
+
   return new Bromise((resolve, reject) => {
     // List all available subtitles
     ffmpeg(inputVideo)
       .ffprobe((err, data) => {
-        console.log(data.streams.filter(stream => stream.codec_type === 'subtitle'));
+        const subStreams = data.streams
+          .filter(stream => stream.codec_type === 'subtitle')
+          .map(stream => ({
+            language: stream.tags.language,
+            index: stream.index
+          }));
+        // subStreams.forEach(language => console.log(language));
+        const languages = subStreams.map(stream => stream.language);
+        inquirer.prompt([
+          {
+            type: 'list',
+            name: 'subtitleLang',
+            choices: languages,
+            message: 'Please choose a text language',
+            default: languages[0]
+          }
+        ])
+        .then(
+          answers => console.log(answers)
+        );
       });
-      resolve();
+
     // Wait for user input
+
     // Return object of requested subtitle file and stream index OR given subtitle files
   });
 };
