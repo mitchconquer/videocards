d6fb0c78f965905b9402350bc7206d216dde2f6d
Mitch Conquer <mitch.conquer@gonimbly.com>
2016-10-29 12:18:36 -0700

    update index.js

--- index.js
+++ index.js
@@ -2,18 +2,18 @@
 
 const initializeSubs = require('./src/initialize');
 const subtitles = require('./src/subtitles');
-const generateAudio = require('./src/audio');
-const utils = require('./src/utils');
-const createAnkiDb = require('./src/database');
-const apkgCreater = require('./src/archiver');
-const Bromise = require('bluebird');
+// const generateAudio = require('./src/audio');
+// const utils = require('./src/utils');
+// const createAnkiDb = require('./src/database');
+// const apkgCreater = require('./src/archiver');
+// const Bromise = require('bluebird');
 
-const userArgs = process.argv.slice(2);
-const inputVideo = userArgs[0];
-const inputSubs = userArgs[1];
+// const userArgs = process.argv.slice(2);
+// const inputVideo = userArgs[0];
+// const inputSubs = userArgs[1];
 
 exports.initializeSubs = (inputSubs, inputVideo) => {
-  let subtiles;
+  let subtitles;
   initializeSubs(inputSubs, inputVideo)
     .then(
       subs => {
@@ -24,24 +24,24 @@ exports.initializeSubs = (inputSubs, inputVideo) => {
   return subtitles;
 };
 
-initializeSubs(inputSubs, inputVideo)
-  .then(subtitles.subsTransform)
-  .then(subtitles.joinSentences)
-  .then(
-    subsData => generateAudio(inputVideo, subsData)
-  )
-  .then(
-    noteData => createAnkiDb(inputVideo, noteData)
-  )
-  .then(
-    dbFile => apkgCreater(dbFile, utils.quickName(inputVideo))
-  )
-  .then(
-    () => {utils.rmFiles('./pkg');}
-  )
-  .then(
-    () => {utils.rmFiles('./output');}
-  )
-  .catch(
-    err => console.log('A big ol\' error occured', err)
-  );
+// initializeSubs(inputSubs, inputVideo)
+//   .then(subtitles.subsTransform)
+//   .then(subtitles.joinSentences)
+//   .then(
+//     subsData => generateAudio(inputVideo, subsData)
+//   )
+//   .then(
+//     noteData => createAnkiDb(inputVideo, noteData)
+//   )
+//   .then(
+//     dbFile => apkgCreater(dbFile, utils.quickName(inputVideo))
+//   )
+//   .then(
+//     () => {utils.rmFiles('./pkg');}
+//   )
+//   .then(
+//     () => {utils.rmFiles('./output');}
+//   )
+//   .catch(
+//     err => console.log('A big ol\' error occured', err)
+//   );
